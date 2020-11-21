d32a15d3143d60be9bbc4b801735dc5cebd8c669
tanvi-12 <69550520+tanvi-12@users.noreply.github.com>
2020-11-21 20:10:15 +1100

    update work

--- mission_to_mars/app.py
+++ mission_to_mars/app.py
@@ -6,7 +6,7 @@ import scrape_mars
 app = Flask(__name__)
 
 # Use PyMongo to establish Mongo connection
-mongo = PyMongo(app, uri="mongodb://localhost:27017/weather_app")
+mongo = PyMongo(app, uri="mongodb://localhost:27017/mars_app")
 
 
 # Route to render index.html template using data from Mongo
@@ -25,10 +25,10 @@ def home():
 def scrape():
 
     # Run the scrape function
-    costa_data = scrape_costa.scrape_info()
+    mars_data = scrape_mars.scrape_info()
 
     # Update the Mongo database using update and upsert=True
-    mongo.db.collection.update({}, costa_data, upsert=True)
+    mongo.db.collection.update({}, mars_data, upsert=True)
 
     # Redirect back to home page
     return redirect("/")
--- mission_to_mars/scrape_mars.py
+++ mission_to_mars/scrape_mars.py
@@ -5,45 +5,96 @@ import time
 
 def init_browser():
     # @NOTE: Replace the path with your actual path to the chromedriver
-    executable_path = {"executable_path": "/usr/local/bin/chromedriver"}
+    executable_path = {"executable_path": "chromedriver"}
     return Browser("chrome", **executable_path, headless=False)
 
 
 def scrape_info():
-    browser = init_browser()
-
-    # Visit visitcostarica.herokuapp.com
-    url = "https://visitcostarica.herokuapp.com/"
+   
+    # MARS NEWS
+    # Visit mars website
+    url = "https://mars.nasa.gov/news/"
     browser.visit(url)
 
-    time.sleep(1)
+    time.sleep(3)
 
     # Scrape page into Soup
     html = browser.html
     soup = bs(html, "html.parser")
 
-    # Get the average temps
-    avg_temps = soup.find('div', id='weather')
+    # Scrape data and view it
+    news = soup.find("li", class_="slide")
 
-    # Get the min avg temp
-    min_temp = avg_temps.find_all('strong')[0].text
+    # Get the title and description of the latest news
+    news_title = news.find("div", class_="content_title").text
+    news_p = news.find("div", class_="article_teaser_body").text
 
-    # Get the max avg temp
-    max_temp = avg_temps.find_all('strong')[1].text
+    # MARS IMAGE
+    # Visit the image url
+    image = "https://www.jpl.nasa.gov/spaceimages/?search=&category=Mars"
+    browser.visit(image)
 
-    # BONUS: Find the src for the sloth image
-    relative_image_path = soup.find_all('img')[2]["src"]
-    sloth_img = url + relative_image_path
+    time.sleep(3)
 
+    # Scrape page into Soup
+    html = browser.html
+    soup = bs(html, "html.parser")
+    
+    # find article for featured image and print
+    img_info = soup.find("li",class_="slide").a["data-fancybox-href"]
+    base_url = "https://www.jpl.nasa.gov"
+    featured_image_url = base_url + img_info
+   
+    # MARS FACTS
+    # Visit the facts url
+    spacefacts = 'https://space-facts.com/mars/'
+    df = pd.read_html(spacefacts)
+    mars_df = df[0]
+    mars_df = mars_df.rename({0: "", 1: "Mars Statistics"}, axis =1).set_index("")
+    mars_df.to_html("mars_facts.html")
+    
+    # MARS HEMISPHERES
+    # Visit the hemisphers url
+    hem = "https://astrogeology.usgs.gov/search/results?q=hemisphere+enhanced&k1=target&v1=Mars"
+    browser.visit(hem)
+
+    time.sleep(3)
+
+    # Scrape page into Soup
+    html = browser.html
+    soup = bs(html, "html.parser") 
+    
+    # Find the image urls
+    img_links= soup.find_all("div", class_="item")
+    hemisphere_image_urls = []
+    
+    for img in img_links:
+        # Get title
+        title = img.find("h3").text
+        # Click onto image url to get better image
+        extra = img.find("a")["href"]
+        base = "https://astrogeology.usgs.gov"
+        get_url = base + extra
+        response = requests.get(get_url)
+        # Soup it up
+        soup = bs(response.text, "html.parser") 
+        # Get full res image
+        full_res_img = soup.find("img", class_="wide-image")["src"]
+        img_url = base + full_res_img
+        hemisphere_image_urls.append({"title": title, "img_url": img_url})
+
+    #STORING DATA
     # Store data in a dictionary
-    costa_data = {
-        "sloth_img": sloth_img,
-        "min_temp": min_temp,
-        "max_temp": max_temp
-    }
+    mars_data = {
+        "news_title": news_title,
+        "news_p": news_p,
+        "featured_image_url": featured_image_url,
+        "mars_facts" : mars_facts,
+        "hemisphere_image_urls" : hemisphere_image_urls
+        }
 
     # Close the browser after scraping
     browser.quit()
 
     # Return results
-    return costa_data
+    return mars_data
